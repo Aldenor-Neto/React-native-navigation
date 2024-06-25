bb63aae9eee696ea9594148b431c56a5dbb96cd6
aldenor-neto <aldenor05062010@gmail.com>
2024-06-25 12:52:10 -0300

    Passando parametros na navegação entre páginas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,15 +3,19 @@ import React from "react";
 import { Text, View, Button } from "react-native";
 
 export default function Home() {
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  return (
-    <View>
-      <Text>Página inicial</Text>
-      <Button
-        title="Sobre"
-        onPress={() => navigation.navigate('Sobre')}
-      />
-    </View>
-  );
+    return (
+        <View>
+            <Text>Página inicial</Text>
+            <Button
+                title="Sobre"
+                onPress={() => navigation.navigate('Sobre', {
+                    nome: 'Aldenor Neto',
+                    dataNascimento: '30/12/1987',
+                    cidade: 'Pacatuba'
+                })}
+            />
+        </View>
+    );
 }
--- src/screens/Sobre.js
+++ src/screens/Sobre.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { Text, View, Button, StyleSheet } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
-export default function Sobre(){
+export default function Sobre() {
 
-const navigation = useNavigation();
+    const navigation = useNavigation();
+    const route = useRoute();
 
-    return(
+    const nome = route.params.nome;
+    const dataNascimento = route.params.dataNascimento;
+    const cidade = route.params.cidade;
+
+    return (
         <View>
-<Text>Página secundaria</Text>
-<Button
-title="Voltar"
-onPress={() => navigation.navigate('Home')}
-/>
+            <Text>Página Sobre</Text>
+            <Text>Dados</Text>
+
+            <Text>Nome: {nome}</Text>
+            <Text>Data de nascimento: {dataNascimento}</Text>
+            <Text>Cidade: {cidade}</Text>
+
+            <Button
+                title="Voltar"
+                onPress={() => navigation.navigate('Home')}
+            />
         </View>
     )
 }
